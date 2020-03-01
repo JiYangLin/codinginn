4eb4bd5b71463674d228514e1e17f70243681863
jiyanglin <760437565@qq.com>
2020-03-02 01:47:42 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: '编码小栈',
+    description: '欢迎光临编码小栈',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/favicon.ico'
+        }]
+    ],
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
+
+
+    themeConfig: {
+        logo:  '/favicon.ico',
+        nav: [
+          { text: '主页', link: '/' },
+          { text: '编程笔记', link: '/note/' },
+          { text: '点点滴滴', link: '/alittle/' },
+          { text: '我的项目', link: '/myprj/' },
+          { text: 'github', link: 'https://github.com/JiYangLin' },
+        ],
+        sidebar: 'auto'//从二级标题开始提取（第一行内容如果是1级标题会提取）
+    }
+
+    
+}
